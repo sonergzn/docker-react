222a75c402584cb23266bcf0ce98ade6a6abea27
Soner Gezgen <soner.gezgen@neoria.be>
2022-10-25 10:11:19 +0200

    important update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi there, Bye there</p>
+        <p>Hi there, Bye there
+          very important update
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
