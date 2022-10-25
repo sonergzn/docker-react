2d990094dc54c61da61a3d4a8a053711d4a3fbe4
Soner Gezgen <soner.gezgen@neoria.be>
2022-10-25 11:00:33 +0200

    final update for opeshift

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi there, Bye there
-          very important update
-          Second important update
+        <p>Hi There.
+          Final update for openshift.
         </p>
         <a
           className="App-link"
